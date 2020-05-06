07a12ea399b02730cdaa6d18cc696572b8a20c5d
Dzianis Pasanen <pasanenD@yandex.by>
2020-05-06 23:38:09 +0300

    Update 2

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -4,6 +4,31 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        var x = 1
+        var res: Long = 0
+        val m = array[0].toLong()
+        val n = array[1]
+        while (x<n){
+            res = factorial(n)/(factorial(x)*factorial(n-x))
+            if (res==m){
+                break
+            }
+            x+=1
+        }
+
+        return if (x==n&&res!=m) {
+            null
+        } else{
+            x
+        }
+    }
+
+    private fun factorial(fac: Int):Long{
+        var factorial: Long = 1
+        for (i in 1..fac) {
+            factorial *= i.toLong()
+        }
+        return factorial
     }
 }
+
