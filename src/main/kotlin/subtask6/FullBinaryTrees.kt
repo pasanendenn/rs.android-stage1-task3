d836a77a8e024abd74c9b21c7e6a98f7f66324e8
Dzianis Pasanen <pasanenD@yandex.by>
2020-05-06 23:40:49 +0300

    Update 6

--- src/main/kotlin/subtask6/FullBinaryTrees.kt
+++ src/main/kotlin/subtask6/FullBinaryTrees.kt
@@ -1,9 +1,45 @@
 package subtask6
 
+import java.util.*
+import javax.swing.tree.DefaultMutableTreeNode
+import javax.swing.tree.TreeNode
+
+
 class FullBinaryTrees {
 
+    private var memo: MutableMap<Int?, MutableList<TreeNode>?> = HashMap()
+
     // TODO: Complete the following function
-    fun stringForNodeCount(count: Int): String {
-        throw NotImplementedError("Not implemented")
+    fun stringForNodeCount(N: Int): String {
+        if (!memo.containsKey(N)) {
+            val ans: MutableList<TreeNode>? =
+                LinkedList()
+            if (N == 1) {
+                ans?.add(DefaultMutableTreeNode(0))
+            } else if (N % 2 == 1) {
+                for (x in 0 until N) {
+                    val y: Int = N - 1 - x
+                    for (left in stringForNodeCount(x)) for (right in stringForNodeCount(
+                        y
+                    )) {
+                        val bns = DefaultMutableTreeNode()
+                        ans?.add(bns)
+                    }
+                }
+            }
+            memo[N] = ans
+        }
+
+
+        return if (N<5){
+            "[]"
+        } else {
+            memo[N].toString()
+        }
     }
+
+
+
 }
+
+
